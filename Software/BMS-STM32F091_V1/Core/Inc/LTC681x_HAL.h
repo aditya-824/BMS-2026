a674706374e204785fd497358cdc882a949b7017
aditya-824 <aditya.bhalerao@somaiya.edu>
2025-07-22 19:22:32 +0530

    Added initial code, LT_SPI library with dependencies conversion completed

---
+++ Software/BMS-STM32F091_V1/Core/Inc/LTC681x_HAL.h
@@ -0,0 +1,67 @@
+/*
+ * LTC681x_HAL.h
+ *
+ *  Created on: Jul 22, 2025
+ *      Author: awnin
+ */
+
+#ifndef INC_LTC681X_HAL_H_
+#define INC_LTC681X_HAL_H_
+
+
+#define MD_422HZ_1KHZ 0
+#define MD_27KHZ_14KHZ 1
+#define MD_7KHZ_3KHZ 2
+#define MD_26HZ_2KHZ 3
+
+#define ADC_OPT_ENABLED 1
+#define ADC_OPT_DISABLED 0
+
+#define CELL_CH_ALL 0
+#define CELL_CH_1and7 1
+#define CELL_CH_2and8 2
+#define CELL_CH_3and9 3
+#define CELL_CH_4and10 4
+#define CELL_CH_5and11 5
+#define CELL_CH_6and12 6
+
+#define SELFTEST_1 1
+#define SELFTEST_2 2
+
+#define AUX_CH_ALL 0
+#define AUX_CH_GPIO1 1
+#define AUX_CH_GPIO2 2
+#define AUX_CH_GPIO3 3
+#define AUX_CH_GPIO4 4
+#define AUX_CH_GPIO5 5
+#define AUX_CH_VREF2 6
+
+#define STAT_CH_ALL 0
+#define STAT_CH_SOC 1
+#define STAT_CH_ITEMP 2
+#define STAT_CH_VREGA 3
+#define STAT_CH_VREGD 4
+
+#define REG_ALL 0
+#define REG_1 1
+#define REG_2 2
+#define REG_3 3
+#define REG_4 4
+#define REG_5 5
+#define REG_6 6
+
+#define DCP_DISABLED 0
+#define DCP_ENABLED 1
+
+#define PULL_UP_CURRENT 1
+#define PULL_DOWN_CURRENT 0
+
+#define NUM_RX_BYT 8
+#define CELL 1
+#define AUX 2
+#define STAT 3
+#define CFGR 0
+#define CFGRB 4
+#define CS_PIN 4
+
+#endif /* INC_LTC681X_HAL_H_ */
---
+++ Software/BMS-STM32F091_V1/Core/Src/LT_HAL_SPI.c
@@ -0,0 +1,105 @@
+/*
+ * LT_HAL_SPI.c
+ *
+ *  Created on: Jul 19, 2025
+ *      Author: awnin
+ */
+
+#include <stdint.h>
+#include "stm32f0xx_hal.h"
+#include "LT_HAL_SPI.h"
+
+#define output_low(pin)   HAL_GPIO_WritePin(CS_GPIO_PORT, pin, RESET)
+#define output_high(pin)  HAL_GPIO_WritePin(CS_GPIO_PORT, pin, SET)
+#define CS_GPIO_PORT GPIOA // GPIO Port for software CS pin
+#define SPI_Handle hspi1
+
+//#define SPI_Handle hspi1
+
+void cs_low(uint8_t pin)
+{
+  output_low(pin);
+}
+
+void cs_high(uint8_t pin)
+{
+  output_high(pin);
+}
+
+void delay_u(uint16_t micro)
+{
+	__HAL_TIM_SET_COUNTER(&htim2,0);  // set the counter value a 0
+		while (__HAL_TIM_GET_COUNTER(&htim2) < micro);  // wait for the counter to reach the us input
+}
+
+void delay_m(uint16_t milli)
+{
+  HAL_Delay(milli);
+}
+
+// Setup the processor for hardware SPI communication.
+// Must be called before using the other SPI routines.
+// Alternatively, call quikeval_SPI_connect(), which automatically
+// calls this function.
+void spi_enable() // Configures SCK frequency. Use constant defined in header file.
+{
+  //pinMode(SCK, OUTPUT);             //! 1) Setup SCK as output
+  //pinMode(MOSI, OUTPUT);            //! 2) Setup MOSI as output
+  //pinMode(QUIKEVAL_CS, OUTPUT);     //! 3) Setup CS as output
+//  SPI.begin();
+//  SPI.setClockDivider(spi_clock_divider);
+}
+
+/*
+Writes an array of bytes out of the SPI port
+*/
+void spi_write_array(uint8_t len, // Option: Number of bytes to be written on the SPI port
+                     uint8_t data[] //Array of bytes to be written on the SPI port
+                    )
+{
+	uint8_t i;
+	uint8_t rx_data;
+
+  for (i = 0; i < len; i++)
+  {
+//	  SPI.transfer((int8_t)data[i]);
+	  HAL_SPI_TransmitReceive(&SPI_Handle, (uint8_t *)&data[i], &rx_data, 1, HAL_MAX_DELAY);
+  }
+}
+
+/*
+ Writes and read a set number of bytes using the SPI port.
+
+*/
+
+void spi_write_read(uint8_t tx_Data[],//array of data to be written on SPI port
+                    uint8_t tx_len, //length of the tx data arry
+                    uint8_t *rx_data,//Input: array that will store the data read by the SPI port
+                    uint8_t rx_len //Option: number of bytes to be read from the SPI port
+                   )
+{
+	uint8_t i;
+	uint8_t data;
+
+  for (i = 0; i < tx_len; i++)
+  {
+//	  SPI.transfer(tx_Data[i]);
+	  HAL_SPI_TransmitReceive(&SPI_Handle, (uint8_t*)&tx_Data[i], &data, 1, HAL_MAX_DELAY);
+  }
+
+  for (uint8_t i = 0; i < rx_len; i++)
+  {
+//    rx_data[i] = (uint8_t)SPI.transfer(0xFF);
+	  HAL_SPI_TransmitReceive(&SPI_Handle, (uint8_t *)&0xFF, (uint8_t*)&rx_data[i], 1, HAL_MAX_DELAY);
+  }
+
+}
+
+
+uint8_t spi_read_byte(uint8_t tx_dat)
+{
+  uint8_t data;
+//  data = (uint8_t)SPI.transfer(0xFF);
+  HAL_SPI_TransmitReceive(&SPI_Handle, (uint8_t *)&0xFF, (uint8_t *)&data, 1, HAL_MAX_DELAY);
+  return(data);
+}
